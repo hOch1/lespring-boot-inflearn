35fb9e4bd2c1a791ac2349dad5d9704c1e13118b
h0ch1 <h0ch1@naver.com>
2021-07-04 13:47:45 +0000

    .

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -1,13 +1,22 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+
 public class MemoryMemberRepositoryTest {
     
     MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
     
     @Test
     public void save(){
@@ -33,5 +42,20 @@ public class MemoryMemberRepositoryTest {
         Member result = repository.findByName("spring1").get();
 
         assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("srping1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("srping1");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(3);
     }
 }
